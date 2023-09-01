dae21db27095e5ce609c2764cb05121a34111fd4
GhostNear <ghostnear1841@gmail.com>
2023-09-01 21:13:07 +0300

    Add euler 15

---
+++ 0015/main.v
@@ -0,0 +1,11 @@
+module main
+
+fn main() {
+	mut grid := [21][21]u64{init: 0}
+	for index_x in 1..21 {
+		for index_y in 1..21 {
+			grid[index_x][index_y] += 1 + grid[index_x - 1][index_y] + grid[index_x][index_y - 1]
+		}
+	}
+	println("${grid[20][20] + 1}")
+}
